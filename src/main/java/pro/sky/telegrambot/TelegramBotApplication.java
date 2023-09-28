98982d526d577259b69ef34ab7c8d7dce25055a7
kaVII-x <ka52jiguli@yandex.ru>
2023-09-28 19:23:13 +0300

    version 1.3

--- src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
+++ src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -1,12 +1,10 @@
 package pro.sky.telegrambot;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@OpenAPIDefinition
 @EnableScheduling
 
 public class TelegramBotApplication {
